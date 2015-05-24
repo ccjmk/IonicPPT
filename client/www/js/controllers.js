77cd858b54ff42dadbfae57208732b9f90cc42a0
ccjmk <iaguastalli@gmail.com>
2015-05-23 18:37:37 -0700

    initial commit

---
+++ client/www/js/controllers.js
@@ -0,0 +1,104 @@
+angular.module('starter.controllers', [])
+
+.controller("HomeCtrl", function($scope, $timeout, $location){
+
+  var status = {
+    ready: {
+      loginButtonText: "Jugar!",
+      loginDisabled: false,
+    },
+    waitingForOponent: {
+      loginButtonText: "Esperando oponente",
+      loginDisabled: true
+    }
+  };
+
+  $scope.username = "";
+  $scope.status = status["ready"];
+  $scope.login = function(){
+    $scope.status = status["waitingForOponent"];
+    $timeout(function(){
+      $location.path("game");
+    },2000);
+  }
+})
+
+.controller('GameCtrl', function($scope, $timeout, $interval){
+
+  var status = {
+    choosing: {
+
+    },
+    waitingOponent: {
+
+    },
+    showingResult: {
+
+    }
+  };
+
+
+  var player1 = {
+    name: "Iñaki",
+    points: 2,
+    selectElement: "scissors"
+  };
+
+
+  var player2 = {
+    name: "Migue",
+    points: 3,
+    selectElement: "scissors"
+  };
+
+  $scope.timeLeft = 0;
+  $scope.players = [player1, player2];
+  $scope.buttonsDisabled = false;
+
+  $scope.doPlay = function(e){
+    alert(e);
+  }
+
+  $interval(function(){
+    $scope.timeLeft += 1;
+    //console.log($scope.timeLeft);
+    $scope.timeLeft = $scope.timeLeft %1000;
+},16);
+
+
+})
+
+
+.directive('stars', function() {
+  return {
+    restrict: 'E',
+    controller: function($scope){
+      $scope.range = function(n){
+        return new Array(n);
+      };
+    },
+    scope: {
+      points: '='
+    },
+    templateUrl: '/templates/stars.html'
+  };
+})
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
