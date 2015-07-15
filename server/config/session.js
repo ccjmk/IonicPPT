57198e8b23a0db2296c75e285626a58cbab05e89
Miguel Galante <mgalante@gmail.com>
2015-07-15 11:59:14 -0300

    Updating Sails

--- server/config/session.js
+++ server/config/session.js
@@ -9,7 +9,7 @@
  * and auto-save to `req.session` with Socket.io the same way you would with Express.
  *
  * For more information on configuring the session, check out:
- * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.session.html
+ * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.session.html
  */
 
 module.exports.session = {
@@ -21,7 +21,7 @@ module.exports.session = {
   * of your users, forcing them to log in again.                             *
   *                                                                          *
   ***************************************************************************/
-  secret: 'd79d2a258cabcb13c7745e65574d9a46',
+  secret: '51aea703cdf02e489d6c86d361357f74',
 
 
   /***************************************************************************
